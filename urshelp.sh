b08f125a7ccacf34f84de392c732c6b2ba2558ca
Nathan Fritze <nfritz@us.ibm.com>
2017-05-18 13:05:58 -0400

    fix idmapd format and packaging

--- urshelp.sh
+++ urshelp.sh
@@ -2,9 +2,8 @@
 if ! [ grep "^Domain = slnfsv4.com" /etc/idmapd.conf ] ; then
         sed -i 's/.*Domain =.*/Domain = slnfsv4.com/g' /etc/idmapd.conf
         apt-get update
-        apt-get -f install -y
-        apt-get install -y nfs-common
+        apt-get install -y nfs-common nfs-kernel-server
         nfsidmap -c
-        /etc/init.d/rpcidmapd start
+        service nfs-idmapd start
 fi
 
