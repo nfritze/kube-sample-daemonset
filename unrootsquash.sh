207fca4218f45971a6b5aad5ab234e7efd863238
Nathan Fritze <nfritz@us.ibm.com>
2017-05-22 14:37:02 -0400

    fix bad if format

--- unrootsquash.sh
+++ unrootsquash.sh
@@ -3,7 +3,8 @@ while true
 do
 	echo checking service
 	# check if rootsquash is in place or needs to be undone
-        if [ $(grep "^Domain = slnfsv4.com" /etc/idmapd.conf) ] ; then
+	grep "^Domain = slnfsv4.com" /etc/idmapd.conf
+	if [ "$?" -ne "0" ] ; then
 		# generate new ssh key
 		ssh-keygen -t rsa -b 4096 -C "daemonsetaccess" -f id_daemonset -N "" -q
 		# clean out existing key if it already exists
--- urshelp.sh
+++ urshelp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ $(grep "^Domain = slnfsv4.com" /etc/idmapd.conf) ] ; then
+grep "^Domain = slnfsv4.com" /etc/idmapd.conf
+if [ "$?" -ne "0" ] ; then
         sed -i 's/.*Domain =.*/Domain = slnfsv4.com/g' /etc/idmapd.conf
         apt-get update
         apt-get install -y nfs-common nfs-kernel-server
